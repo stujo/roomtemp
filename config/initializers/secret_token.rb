ad22500f79a2c8a64dc0b5e58cb6f1e238271a3c
stujo <wordpress@imagescript.com>
2014-03-27 14:06:13 -0700

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Roomtemp::Application.config.secret_key_base = '7e57404c907ed76c6913c40444e1626a512d1015a0b84118b893f70a74777e4562d0476dc1d23a66777d46b20ae5aad78c8d627d063a8bff1b80b806ad142c91'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Roomtemp::Application.config.session_store :cookie_store, key: '_roomtemp_session'
